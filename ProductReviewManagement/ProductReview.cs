a99894cc258c62d11803a50e406b39929e66fec7
Kirti Swaraj <Sai@KirtiSwaraj>
2020-11-08 22:34:02 +0530

    [kirit] Add. Code to create Product review with various fields

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProductReview.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kirti Swaraj"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ProductReviewManagement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// UC 1 : Class having mentioned properties
+    /// </summary>
+    class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kirti Swaraj"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ProductReviewManagement
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management!\n");
+            //UC 1
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1,UserID=101,Rating=5,Review="Excellent",IsLike=true },
+                new ProductReview(){ProductID=3,UserID=106,Rating=5,Review="Excellent",IsLike=true },
+                new ProductReview(){ProductID=2,UserID=102,Rating=4,Review="Good     ",IsLike=true },
+                new ProductReview(){ProductID=4,UserID=107,Rating=4,Review="Good     ",IsLike=true },
+                new ProductReview(){ProductID=3,UserID=103,Rating=3,Review="Average  ",IsLike=true },
+                new ProductReview(){ProductID=2,UserID=108,Rating=3,Review="Average  ",IsLike=true },
+                new ProductReview(){ProductID=4,UserID=104,Rating=2,Review="Satisfactory",IsLike=false },
+                new ProductReview(){ProductID=1,UserID=109,Rating=2,Review="Satisfactory",IsLike=false },
+                new ProductReview(){ProductID=5,UserID=105,Rating=1,Review="Poor     ",IsLike=false },
+                new ProductReview(){ProductID=5,UserID=110,Rating=1,Review="Poor     ",IsLike=false }
+            };
+            //Printing contents of the list
+            foreach (var v in productReviewList)
+            {
+                Console.WriteLine($"ProductID:{v.ProductID}\tUserID:{v.UserID}\tRating:{v.Rating}\tReview:{v.Review}\tIsLike:{v.IsLike}");
+            }
+        }
+    }
+}
